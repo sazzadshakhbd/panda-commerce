1abba2354142fca6ca780c667632ef6aa93ad4f2
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-15 18:13:52 +0600

    background color tomato add for backpack section

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // All h2 color lightblue 
 const colorLightBlue = document.getElementsByTagName('h2');
-// const colorLightBlues = document.querySelectorAll('h2');
-
 for (color of colorLightBlue) {
     color.style.color = 'lightblue';
 }
 
+// Backpack section background color tomato
+
+const bgTomato = document.getElementById('backpack');
+bgTomato.style.backgroundColor = 'tomato'; 
