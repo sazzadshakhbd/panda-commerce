eb05af56d75509a31b30140072bd803060a0fca1
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-08-15 17:59:35 +0600

    color lightblue add for all h2 tags

---
+++ main.js
@@ -0,0 +1,8 @@
+// All h2 color lightblue 
+const colorLightBlue = document.getElementsByTagName('h2');
+// const colorLightBlues = document.querySelectorAll('h2');
+
+for (color of colorLightBlue) {
+    color.style.color = 'lightblue';
+}
+
